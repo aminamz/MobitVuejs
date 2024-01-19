5fa1ec2ca03f4f86d735e7a8312bbed11653964c
Amin Amouzande <aaprojects00@gmail.com>
2024-01-19 12:29:28 +0330

    Creating Header Base Component

---
+++ src/components/MenuBar.vue
@@ -0,0 +1,32 @@
+<script setup>
+import Logo from './MenuItems/Logo.vue';
+import MenuItem from './MenuItems/MenuItem.vue';
+import Search from './MenuItems/Search.vue';
+import LeftSide from './MenuItems/LeftSide.vue';
+</script>
+
+<template>
+  <header class="desktop-menu">
+    <div class="right">
+        <Logo />
+        <MenuItem />
+        <Search />
+    </div>
+    <div class="left">        
+        <LeftSide/>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+.desktop-menu{
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0.5rem;
+    position: sticky;
+    color:var(--color-text-light);    
+}
+
+</style>
